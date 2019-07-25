20eb3e446e512b3b2cb8477d1d45cb3dd67f58b0
lauraprogramacionweb86 <50987354+lauraprogramacionweb86@users.noreply.github.com>
2019-07-25 13:36:07 +0200

    subo archivo

---
+++ temaprestashop.php
@@ -0,0 +1,11 @@
+/*
+ * Custom code goes here.
+ * A template should always ship with an empty custom.css
+ */
+ #header{ background: red !important; }
+
+.carousel-inner .caption{
+    background-color: rgba(0,0,0,.5);
+    
+
+}
